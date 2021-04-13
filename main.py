651aa5e6606eab127c4e67464cb85e29898de81f
www338 <59461656+www338@users.noreply.github.com>
2021-04-13 21:21:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -67,8 +67,8 @@ def main(user, passwd, step):
         return
     
     if step == '':
-        print ("已设置为随机步数（10000-19999）")
-        step = str(random.randint(10000,19999))
+        print ("已设置为随机步数（7000-10100）")
+        step = str(random.randint(7000,10100))
     login_token = 0
     login_token,userid = login(user,password)
     if login_token == 0:
